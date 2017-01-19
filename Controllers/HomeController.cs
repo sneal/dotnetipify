e71787ce849651c755acf969bc2c403af9ff528a
Shawn Neal <sneal@sneal.net>
2017-01-19 10:46:43 -0800

    Add get client IP

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 using System.Net.Http;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Primitives;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotNetIpify
@@ -8,6 +11,7 @@ namespace DotNetIpify
         public IActionResult Index()
         {
             ViewBag.ServerIP = GetServerPublicIP();
+            ViewBag.ClientIP = GetClientPublicIP();
             return View();
         }
 
@@ -21,6 +25,50 @@ namespace DotNetIpify
                 return result.Result;
             }
             return "Unknown";
+        }
+
+        // Based on SO answer http://stackoverflow.com/a/36316189
+        private string GetClientPublicIP()
+        {
+            string ip = SplitCsv(GetHeaderValue("X-Forwarded-For")).FirstOrDefault();
+            if (string.IsNullOrWhiteSpace(ip) && HttpContext?.Connection?.RemoteIpAddress != null)
+            {
+                ip = HttpContext.Connection.RemoteIpAddress.ToString();
+            }
+            if (string.IsNullOrWhiteSpace(ip))
+            {
+                ip = GetHeaderValue("REMOTE_ADDR");
+            }
+            if (string.IsNullOrWhiteSpace(ip))
+            {
+                ip = "Unknown";
+            }
+            return ip;
+        }
+
+        private string GetHeaderValue(string headerName)
+        {
+            StringValues values;
+            if (HttpContext?.Request?.Headers?.TryGetValue(headerName, out values) ?? false)
+            {
+                return values.ToString();
+            }
+            return "";
+        }
+
+        private IList<string> SplitCsv(string csvList)
+        {
+            if (string.IsNullOrWhiteSpace(csvList))
+            {
+                return new List<string>();
+            }
+
+            return csvList
+                .TrimEnd(',')
+                .Split(',')
+                .AsEnumerable<string>()
+                .Select(s => s.Trim())
+                .ToList();
         }
     }
 }
