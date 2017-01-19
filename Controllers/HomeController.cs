046f549c6537a3205cf15f4d72d164c6b57b2356
Shawn Neal <sneal@sneal.net>
2017-01-19 10:20:08 -0800

    Reformat tabs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,15 +3,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DotNetIpify
 {
-  public class HomeController : Controller {
-      public IActionResult Index()
-      {
-          var client = new HttpClient();
-          client.DefaultRequestHeaders.Accept.Clear();
-          var result = client.GetStringAsync("https://api.ipify.org?format=text");
-          result.Wait(5000);
-          ViewBag.ServerIP = result.Result;
-          return View();
-      }
-  }
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            var result = client.GetStringAsync("https://api.ipify.org?format=text");
+            result.Wait(5000);
+            ViewBag.ServerIP = result.Result;
+            return View();
+        }
+    }
 }
